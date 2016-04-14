79f7ae3f77681001e1814fc59d047d6ae7a7c13f
Amandeep Singh <amandeep.s.saggu@gmail.com>
2016-04-14 11:38:36 -0700

    add exception handling

--- EntityResolution/EntityResolution/EntityResolution/test.py
+++ EntityResolution/EntityResolution/EntityResolution/test.py
@@ -3,58 +3,66 @@ import faerie1
 from pyspark.sql import Row
 import Toolkit
 import time
+import os,sys
 
 def processDoc(line, d):
-    uri = line["uri"]
-    city = line["locality"]
-    state = line["region"]
-    country = line["country"]
-    start_time = time.clock()
+    if line and 'uri' in line:
+        uri = line["uri"]
+        city = line["locality"]
+        state = line["region"]
+        country = line["country"]
+        start_time = time.clock()
+        print 'in test.processDoc'
+        country_can = faerie1.processDoc2(uri, country, d.value.all_faerie_dict["countries_dict"])
 
-    country_can = faerie1.processDoc2(uri, country, d.value.all_faerie_dict["countries_dict"])
+        cities_can = search(country_can, uri, state, city, d)
+        process_time = str((time.clock() - start_time)*1000)
+        print "Time take to process: " + json.dumps(line) + " is " + process_time
+        jsent = []
+        if cities_can and 'entities' in cities_can:
+            for eid in cities_can["entities"]:
+                entity = cities_can["entities"][eid]
+                snc = Toolkit.get_value_json(eid + "$snc", d.value.all_city_dict,'$')
+                if snc != '':
+                    temp = Row(id=eid,value=entity["value"] + ","+snc,candwins=entity["candwins"])
+                    jsent.append(temp)
 
-    cities_can = search(country_can, uri, state, city, d)
-    process_time = str((time.clock() - start_time)*1000)
-    print "Time take to process: " + json.dumps(line) + " is " + process_time
-    jsent = []
-    if cities_can and 'entities' in cities_can:
-        for eid in cities_can["entities"]:
-            entity = cities_can["entities"][eid]
-            snc = Toolkit.get_value_json(eid + "$snc", d.value.all_city_dict,'$')
-            if snc != '':
-                temp = Row(id=eid,value=entity["value"] + ","+snc,candwins=entity["candwins"])
-                jsent.append(temp)
-
-        jsdoc = Row(id=cities_can["document"]["id"],value=cities_can["document"]["value"] + ","+state+","+country)
-        jsonline = Row(document=jsdoc,entities=jsent, processtime=process_time)
-        return jsonline
-    else:
-        "cities_can has no entities:", city + "," + state + "," + country + "," + uri
+            jsdoc = Row(id=cities_can["document"]["id"],value=cities_can["document"]["value"] + ","+state+","+country)
+            jsonline = Row(document=jsdoc,entities=jsent, processtime=process_time)
+            return jsonline
+        else:
+            "cities_can has no entities:", city + "," + state + "," + country + "," + uri
 
     return ''
 
 def search(country_can, uri, state, city, d):
     states_can = {}
     cities_can = {}
-    if country_can and country_can != {} and country_can["entities"] != {}:
-        for country_uri in country_can["entities"]:
+    try:
+        if country_can and country_can != {} and country_can["entities"] != {}:
+            for country_uri in country_can["entities"]:
+                if state != "":
+                    if 'entities' in states_can:
+                        states_can["entities"] = dict(states_can["entities"],
+                                              **faerie1.processDoc2(uri, state, d.value.all_faerie_dict[country_uri]["states_dict"])[
+                                                      "entities"])
+                    else:
+                        states_can = faerie1.processDoc2(uri, state, d.value.all_faerie_dict[country_uri]["states_dict"])
+                if states_can == None or states_can == {} or states_can["entities"] == {}:
+                    # if input state is empty, get the state uris from all_dicts in that country
+                    states_can["entities"] = d.value.all_faerie_dict[country_uri]["states_dict"][4].values()
+
+                cities_can = searchcity(states_can,uri,city, d)
+        else:
             if state != "":
-                if 'entities' in states_can:
-                    states_can["entities"] = dict(states_can["entities"],
-                                          **faerie1.processDoc2(uri, state, d.value.all_faerie_dict[country_uri]["states_dict"])[
-                                                  "entities"])
-                else:
-                    states_can = faerie1.processDoc2(uri, state, d.value.all_faerie_dict[country_uri]["states_dict"])
-            if states_can == None or states_can == {} or states_can["entities"] == {}:
-                # if input state is empty, get the state uris from all_dicts in that country
-                states_can["entities"] = d.value.all_faerie_dict[country_uri]["states_dict"][4].values()
+                states_can = faerie1.processDoc2(uri, state, d.value.state_faerie_dict)
 
-            cities_can = searchcity(states_can,uri,city, d)
-    else:
-        if state != "":
-            states_can = faerie1.processDoc2(uri, state, d.value.state_faerie_dict)
-
-        cities_can = searchcity(states_can, uri, city, d)
+            cities_can = searchcity(states_can, uri, city, d)
+    except:
+        print country_can
+        print uri
+        print state
+        print city
 
     return cities_can
 
@@ -80,7 +88,9 @@ def searchcity(states_can, uri, city, d):
     return cities_can
 
 def run(d, input_rdd):
-
-    candidates = input_rdd.map(lambda line : processDoc(json.loads(line), d))
+    os.environ['PYTHON_EGG_CACHE'] = '/tmp'
+    sys.path.append(os.environ['PYTHON_EGG_CACHE'])
+    print 'in test.run'
+    candidates = input_rdd.filter(lambda x:x is not None).map(lambda line : processDoc(json.loads(line), d))
     return candidates
 
